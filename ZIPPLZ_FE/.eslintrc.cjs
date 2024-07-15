5fe7f7be031277c259062d868fd492e2584f49f3
HannaKang <hanna0175@gmail.com>
2024-07-16 00:49:11 +0900

    ADD: ESLint configuration

--- ZIPPLZ_FE/.eslintrc.cjs
+++ ZIPPLZ_FE/.eslintrc.cjs
@@ -1,6 +1,9 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
@@ -9,7 +12,7 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@typescript-eslint'],
   rules: {
     'prettier/prettier': [
       'error',
@@ -17,5 +20,10 @@ module.exports = {
         endOfLine: 'auto',
       },
     ],
+    'prefer-arrow-callback': 'error',
+    'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
+    'consistent-return': 'error',
+    'no-var': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'off',
   },
 };
