8d2e0e672af8607907fc7573a2c68b3020816f1e
HannaKang <hanna0175@gmail.com>
2024-07-16 02:12:50 +0900

    Remove: delete unnecessary files

--- ZIPPLZ_FE/.prettierrc.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  singleQuote: true,
-  useTabs: false,
-  printWidth: 80,
-  arrowParens: 'always',
-  trailingComma: 'es5',
-  tabWidth: 2,
-  semi: true,
-};
--- ZIPPLZ_FE/src/pages/WorkerList.tsx
+++
@@ -1,3 +0,0 @@
-export const WorkerList = () => {
-  return <div>WorkerList</div>;
-};
