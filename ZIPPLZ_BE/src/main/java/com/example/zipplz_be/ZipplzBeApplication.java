f7db413a88d0914dafaccba24d036929d82ceafd
Gaeun Lee <rk243812@gmail.com>
2024-07-15 14:38:30 +0900

    FEAT: create back project

---
+++ ZIPPLZ_BE/src/main/java/com/example/zipplz_be/ZipplzBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.zipplz_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipplzBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipplzBeApplication.class, args);
+	}
+
+}
