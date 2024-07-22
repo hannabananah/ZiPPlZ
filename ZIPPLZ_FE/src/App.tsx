5fd6536708969b911b2f9fa03eda8880362ef75a
kht19684 <kht19684@gmail.com>
2024-07-22 15:02:18 +0900

    FEAT: create Button Component - props:{children, type, width, height, fontSize, radius, onClick function}

--- ZIPPLZ_FE/src/App.tsx
+++ ZIPPLZ_FE/src/App.tsx
@@ -1,5 +1,7 @@
 import { Link, Outlet, Route, Routes } from 'react-router-dom';
 
+import Button from '@components/common/Button';
+
 import Layout from './components/layout/Layout';
 import NotFound from './pages/common/NotFound';
 import Home from './pages/common/home/Home';
@@ -8,11 +10,20 @@ import MyPage from './pages/common/mypage/MyPage';
 export default function App() {
   return (
     <>
+      <p>hello</p>
+      <Button
+        children="primary"
+        buttonType="primary"
+        width={6}
+        height={3}
+        fontSize="lg"
+        radius="btn"
+        onClick={() => alert('clicked')}
+      />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="mypage" element={<MyPage />} />
-
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
--- ZIPPLZ_FE/src/components/common/Input.tsx
+++ ZIPPLZ_FE/src/components/common/Input.tsx
@@ -1,3 +1,12 @@
+interface Props {
+  children: string;
+  buttonType: 'normal' | 'light' | 'second' | 'primary';
+  width: number;
+  height: number;
+  fontSize: string;
+  radius: string;
+  onClick?: () => void;
+}
 export default function Input() {
   return <div>Input</div>;
 }
