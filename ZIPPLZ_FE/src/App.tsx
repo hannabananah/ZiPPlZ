d9f6683ad41a60b826e2590b97ee397241c01f1a
kht19684 <kht19684@gmail.com>
2024-07-22 18:03:46 +0900

    FEAT:스케줄러카드 진행중

--- ZIPPLZ_FE/src/App.tsx
+++ ZIPPLZ_FE/src/App.tsx
@@ -5,41 +5,24 @@ import Button from '@components/common/Button';
 import Input from '@components/common/Input';
 
 import Layout from './components/layout/Layout';
+import SchedulerCard from './components/scheduler/SchedulerCard';
 import NotFound from './pages/common/NotFound';
 import Home from './pages/common/home/Home';
 import MyPage from './pages/common/mypage/MyPage';
 
 export default function App() {
   return (
-    <>
-      <p>hello</p>
-      <Button
-        children="primary"
-        buttonType="primary"
-        width={6}
-        height={2}
-        fontSize="lg"
-        radius="none"
-        onClick={() => alert('clicked')}
-      />
-      <Input
-        placeholder="이메일을 입력해주세요"
-        inputType="signup"
-        width={10}
-        height={3}
-        fontSize="xs"
-        radius="none"
-        onKeydown={(e: React.KeyboardEvent<HTMLInputElement>) => {
-          if (e.key === 'Enter') alert('keydown');
-        }}
-      />
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="mypage" element={<MyPage />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </>
+    <div>
+      <SchedulerCard />
+      <>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="mypage" element={<MyPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </>
+    </div>
   );
 }
