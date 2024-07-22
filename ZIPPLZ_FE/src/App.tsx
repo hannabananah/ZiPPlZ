2c62d101c582d956e1d2f1d2b1d1c64bd6a008ba
kht19684 <kht19684@gmail.com>
2024-07-22 18:03:46 +0900

    FEAT:스케줄러카드 진행중

--- ZIPPLZ_FE/src/App.tsx
+++ ZIPPLZ_FE/src/App.tsx
@@ -1,14 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 import VideoRoom from '@pages/common/chat/VideoRoom';
-=======
-import Button from '@components/common/Button';
->>>>>>> c770e44 (FEAT: create Button Component)
 
-=======
->>>>>>> 7e3dddf (FEAT: create Input Component)
 import Layout from './components/layout/Layout';
 import NotFound from './pages/common/NotFound';
 import ChatRoom from './pages/common/chat/ChatRoom';
@@ -18,7 +11,6 @@ import Schedule from './pages/user/Schedule';
 
 export default function App() {
   return (
-<<<<<<< HEAD
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
@@ -30,16 +22,5 @@ export default function App() {
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
-=======
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="mypage" element={<MyPage />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </>
->>>>>>> c770e44 (FEAT: create Button Component)
   );
 }
