28dedd90416f6ab7dfa73f1e20499b070f5c54c6
KIMHT98 <kht19684@gmail.com>
2024-07-22 23:38:31 +0900

    FEAT: svg icons tsx파일 추가

---
+++ ZIPPLZ_FE/src/assets/svg/icons/index.ts
@@ -0,0 +1,12 @@
+export { default as ArrowLeftIcon } from './ArrowLeftIcon';
+export { default as BookMarkIcon } from './BookMarkIcon';
+export { default as ChatIcon } from './ChatIcon';
+export { default as ChevronDownIcon } from './ChevronDownIcon';
+export { default as ChevronLeftIcon } from './ChevronLeftIcon';
+export { default as ChevronRightIcon } from './ChevronRightIcon';
+export { default as ChevronUpIcon } from './ChevronUpIcon';
+
+export { default as InvisibleIcon } from './InvisibleIcon';
+export { default as TrashCanIcon } from './TrashCanIcon';
+export { default as VisibleIcon } from './VisibleIcon';
+export { default as MeetballIcon } from './MeetballIcon';
