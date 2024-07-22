f13dbabd73a6650fc0a2c851cf4efc451b533636
KIMHT98 <kht19684@gmail.com>
2024-07-23 00:48:06 +0900

    FEAT: scheduler 페이지 작업중

--- ZIPPLZ_FE/src/App.tsx
+++ ZIPPLZ_FE/src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
 import { Link, Outlet, Route, Routes } from 'react-router-dom';
 
-import Button from '@components/common/Button';
-import Input from '@components/common/Input';
-
 import Layout from './components/layout/Layout';
-import SchedulerCard from './components/scheduler/SchedulerCard';
 import NotFound from './pages/common/NotFound';
 import Home from './pages/common/home/Home';
 import MyPage from './pages/common/mypage/MyPage';
+import Schedule from './pages/user/Schedule';
 
 export default function App() {
   return (
     <div>
-      <SchedulerCard />
       <>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="mypage" element={<MyPage />} />
             <Route path="*" element={<NotFound />} />
+            <Route path="schedule" element={<Schedule />} />
           </Route>
         </Routes>
       </>
--- ZIPPLZ_FE/src/pages/user/Schedule.tsx
+++ ZIPPLZ_FE/src/pages/user/Schedule.tsx
@@ -1,3 +1,26 @@
+import Calendar from '@/components/scheduler/Calendar';
+import SharedImg from '@/components/scheduler/sharedFile/SharedImg';
+import SharedMemo from '@/components/scheduler/sharedFile/SharedMemo';
+import SchedulerCard from '@components/scheduler/SchedulerCard';
+
 export default function Schedule() {
-  return <div>Schedule</div>;
+  return (
+    <div className="flex justify-center items-center">
+      <div
+        className="flex flex-col justify-center items-center bg-zp-light-beige gap-4 px-6"
+        style={{ width: '37.5rem' }}
+      >
+        <Calendar />
+        <p className="w-full text-ㅣㄷㄹㅅ text-zp-xl font-bold ">공유 문서</p>
+        <div className="w-full flex justify-between items-center">
+          <SharedImg />
+          <SharedMemo />
+        </div>
+        <p className="w-full text-right text-zp-xl font-bold ">
+          총 시공 가격 : 1000000원
+        </p>
+        <SchedulerCard />
+      </div>
+    </div>
+  );
 }
