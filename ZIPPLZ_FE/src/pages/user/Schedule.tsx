94a34a93293edb0cec8f6fc80d135203a933fa28
KIMHT98 <kht19684@gmail.com>
2024-07-24 03:20:05 +0900

    FEAT:스케줄 페이지 디자인 반응형 구현

--- ZIPPLZ_FE/src/pages/user/Schedule.tsx
+++ ZIPPLZ_FE/src/pages/user/Schedule.tsx
@@ -1,18 +1,33 @@
+import { useState } from 'react';
+
 import Calendar from '@/components/common/Calendar';
+import Selectbar from '@/components/common/Selectbar';
 import SchedulerCardExist from '@/components/scheduler/card/SchedulerCardExist';
 import SharedImg from '@/components/scheduler/sharedFile/SharedImg';
 import SharedMemo from '@/components/scheduler/sharedFile/SharedMemo';
 import SchedulerCard from '@components/scheduler/card/SchedulerCard';
 
 export default function Schedule() {
+  const options: string[] = ['집1', '집2', '집3'];
+  const [selectedValue, setSelectedValue] = useState<string>(options[0]);
   return (
-    <div
-      className="flex flex-col justify-center items-center bg-zp-light-beige gap-4 px-6"
-      style={{ width: '37.5rem' }}
-    >
+    <div className="flex flex-col items-center bg-zp-light-beige gap-4 sm px-2 lg px-4">
+      <Selectbar
+        fontColor="main"
+        options={options}
+        selectedValue={selectedValue}
+        setSelectedValue={setSelectedValue}
+        width="full"
+        height={2}
+        fontSize="lg"
+        radius="btn"
+        border="main"
+        hover="sub"
+        backgroundColor="white"
+      />
       <Calendar />
-      <p className="w-full  text-zp-xl font-bold ">공유 문서</p>
-      <div className="w-full flex justify-between items-center">
+      <p className="w-full text-zp-xl font-bold ">공유 문서</p>
+      <div className="flex justify-between items-center gap-4">
         <SharedImg />
         <SharedMemo />
       </div>
