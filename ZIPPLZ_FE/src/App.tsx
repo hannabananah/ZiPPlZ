ab0d69db3523040897efbcc8d7e12c85d9a494e9
HannaKang <hanna0175@gmail.com>
2024-07-23 15:48:27 +0900

    ADD: 채팅방 리스트 페이지, 모달로 변경 및 모달 연결

--- ZIPPLZ_FE/src/App.tsx
+++ ZIPPLZ_FE/src/App.tsx
@@ -3,7 +3,6 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from '@components/layout/Layout';
 import NotFound from '@pages/common/NotFound';
 import ChatRoom from '@pages/common/chat/ChatRoom';
-import ChatRooms from '@pages/common/chat/ChatRooms';
 import Home from '@pages/common/home/Home';
 import MyPage from '@pages/common/mypage/MyPage';
 
@@ -14,7 +13,6 @@ export default function App() {
         <Route index element={<Home />} />
         <Route path="mypage" element={<MyPage />} />
         <Route path="chatrooms/:roomId" element={<ChatRoom />} />
-        <Route path="chatrooms" element={<ChatRooms />}></Route>
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
--- ZIPPLZ_FE/src/components/layout/FloatingChatButton.tsx
+++ ZIPPLZ_FE/src/components/layout/FloatingChatButton.tsx
@@ -1,24 +1,34 @@
+import { IoIosClose } from 'react-icons/io';
 import { PiChatsCircleFill } from 'react-icons/pi';
-import { useNavigate } from 'react-router-dom';
 
 import Circle from '@assets/gradient-circle.svg?react';
+import FullModal from '@components/common/FullModal';
+import { useChatRoomsModalState, useModalActions } from '@store/modalStore';
 
 export default function FloatingChatButton() {
-  const navigate = useNavigate();
+  const { changeModalState } = useModalActions();
+  const chatRoomsModal = useChatRoomsModalState();
+
+  const handleToggleModal = () => {
+    changeModalState('chatRooms');
+  };
 
   return (
     <>
       <button
-        onClick={() => {
-          navigate('/chatrooms');
-        }}
+        onClick={handleToggleModal}
         className="absolute right-4 drop-shadow-zp-deep bottom-24"
       >
         <Circle />
         <div className="absolute transform -translate-x-1/2 -translate-y-[14px] top-1/2 left-1/2">
-          <PiChatsCircleFill size={24} />
+          {chatRoomsModal ? (
+            <IoIosClose size={24} />
+          ) : (
+            <PiChatsCircleFill size={24} />
+          )}
         </div>
       </button>
+      <FullModal isOpen={chatRoomsModal} onRequestClose={handleToggleModal} />
     </>
   );
 }
