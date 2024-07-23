8615079adc3ef3634806944633725417074c87f0
HannaKang <hanna0175@gmail.com>
2024-07-23 10:47:00 +0900

    Add: 공통 컴포넌트 예제 코드 이동 및 input 컴포넌트 코드 추가, 라우트 코드 변경

--- ZIPPLZ_FE/src/App.tsx
+++ ZIPPLZ_FE/src/App.tsx
@@ -4,10 +4,6 @@ import VideoRoom from '@pages/common/chat/VideoRoom';
 
 import Layout from './components/layout/Layout';
 import NotFound from './pages/common/NotFound';
-<<<<<<< HEAD
-=======
-import ChatList from './pages/common/chat/ChatList';
->>>>>>> 8515700 (ADD: 중간 커밋)
 import ChatRoom from './pages/common/chat/ChatRoom';
 import Home from './pages/common/home/Home';
 import MyPage from './pages/common/mypage/MyPage';
@@ -15,7 +11,6 @@ import Schedule from './pages/user/Schedule';
 
 export default function App() {
   return (
-<<<<<<< HEAD
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
@@ -27,33 +22,5 @@ export default function App() {
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
-=======
-<<<<<<< HEAD
-    <div>
-      <>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="mypage" element={<MyPage />} />
-            <Route path="*" element={<NotFound />} />
-            <Route path="schedule" element={<Schedule />} />
-          </Route>
-        </Routes>
-      </>
-    </div>
-=======
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="mypage" element={<MyPage />} />
-          <Route path="chatlist" element={<ChatList />} />
-          <Route path="chatroom/:roomId" element={<ChatRoom />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </>
->>>>>>> 5e99630 (ADD: 중간 커밋)
->>>>>>> 8515700 (ADD: 중간 커밋)
   );
 }
--- ZIPPLZ_FE/src/pages/common/home/Home.tsx
+++ ZIPPLZ_FE/src/pages/common/home/Home.tsx
@@ -1,3 +1,5 @@
+import Button from '@components/common/Button';
+import Input from '@components/common/Input';
 import ScheduleCalendar from '@components/common/calendar/ScheduleCalendar';
 import Calendar from '@components/schedule/Calendar';
 
@@ -5,7 +7,28 @@ export default function Home() {
   return (
     <div>
       Home
-      <ScheduleCalendar />
+      <p>hello</p>
+      <Button
+        children="primary"
+        buttonType="primary"
+        width={6}
+        height={2}
+        fontSize="lg"
+        radius="none"
+        onClick={() => alert('clicked')}
+      />
+      <Input
+        type="text"
+        placeholder="이메일을 입력해주세요"
+        inputType="signup"
+        width={10}
+        height={3}
+        fontSize="xs"
+        radius="none"
+        onKeydown={(e: React.KeyboardEvent<HTMLInputElement>) => {
+          if (e.key === 'Enter') alert('keydown');
+        }}
+      />
     </div>
   );
 }
