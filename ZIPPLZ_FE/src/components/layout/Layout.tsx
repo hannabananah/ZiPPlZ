33ac186de1ab131e5baf9a8c02617038da969f03
HannaKang <hanna0175@gmail.com>
2024-07-23 10:49:59 +0900

    Add: 레이아웃 추가 수정

--- ZIPPLZ_FE/src/components/layout/Layout.tsx
+++ ZIPPLZ_FE/src/components/layout/Layout.tsx
@@ -1,6 +1,5 @@
 import { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
 import BottomTab from '@components/layout/BottomTab';
 import FloatingChatButton from '@components/layout/FloatingChatButton';
@@ -11,21 +10,21 @@ export default function Layout() {
   const location = useLocation();
   const path = location.pathname;
 
+  const notShowHeader = path.startsWith('/chatrooms');
+
   return (
     <Suspense>
-      {
-        <div className="flex flex-col items-center min-h-screen">
-          <div className="flex flex-col relative w-[600px] min-h-screen">
-            {path !== '/chatlist' && path !== '/chatroom' && <Header />}
-            <div className="flex-1 h-full justify-self-center bg-zp-light-yellow">
-              <Outlet />
-            </div>
-            <FloatingChatButton />
-            <BottomTab />
-            {(path === '/' || path === '/mypage') && <Footer />}
+      <div className="flex flex-col items-center justify-center">
+        <div className="relative flex flex-col min-h-screen">
+          {!notShowHeader && <Header />}
+          <div className="flex-1 h-full justify-self-center bg-zp-light-yellow">
+            <Outlet />
           </div>
+          {!notShowHeader && <FloatingChatButton />}
+          <BottomTab />
+          {(path === '/' || path === '/mypage') && <Footer />}
         </div>
-      }
+      </div>
     </Suspense>
   );
 }
