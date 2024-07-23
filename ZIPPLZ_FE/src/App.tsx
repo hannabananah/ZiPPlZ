3b81fd142b458d7d966e5e602fbfc091e5ba6d09
HannaKang <hanna0175@gmail.com>
2024-07-23 10:47:00 +0900

    Add: 공통 컴포넌트 예제 코드 이동 및 input 컴포넌트 코드 추가, 라우트 코드 변경

--- ZIPPLZ_FE/src/App.tsx
+++ ZIPPLZ_FE/src/App.tsx
@@ -1,48 +1,22 @@
 import { Route, Routes } from 'react-router-dom';
 
-import Button from '@components/common/Button';
-import Input from '@components/common/Input';
-
-import Layout from './components/layout/Layout';
-import NotFound from './pages/common/NotFound';
-import ChatList from './pages/common/chat/ChatList';
-import ChatRoom from './pages/common/chat/ChatRoom';
-import Home from './pages/common/home/Home';
-import MyPage from './pages/common/mypage/MyPage';
+import Layout from '@components/layout/Layout';
+import NotFound from '@pages/common/NotFound';
+import ChatRoom from '@pages/common/chat/ChatRoom';
+import ChatRooms from '@pages/common/chat/ChatRooms';
+import Home from '@pages/common/home/Home';
+import MyPage from '@pages/common/mypage/MyPage';
 
 export default function App() {
   return (
-    <>
-      <p>hello</p>
-      <Button
-        children="primary"
-        buttonType="primary"
-        width={6}
-        height={2}
-        fontSize="lg"
-        radius="none"
-        onClick={() => alert('clicked')}
-      />
-      <Input
-        placeholder="이메일을 입력해주세요"
-        inputType="signup"
-        width={10}
-        height={3}
-        fontSize="xs"
-        radius="none"
-        onKeydown={(e: React.KeyboardEvent<HTMLInputElement>) => {
-          if (e.key === 'Enter') alert('keydown');
-        }}
-      />
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="mypage" element={<MyPage />} />
-          <Route path="chatlist" element={<ChatList />} />
-          <Route path="chatroom/:roomId" element={<ChatRoom />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="mypage" element={<MyPage />} />
+        <Route path="chatrooms/:roomId" element={<ChatRoom />} />
+        <Route path="chatrooms" element={<ChatRooms />}></Route>
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
--- ZIPPLZ_FE/src/pages/common/home/Home.tsx
+++ ZIPPLZ_FE/src/pages/common/home/Home.tsx
@@ -1,3 +1,32 @@
+import Button from '@components/common/Button';
+import Input from '@components/common/Input';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <p>hello</p>
+      <Button
+        children="primary"
+        buttonType="primary"
+        width={6}
+        height={2}
+        fontSize="lg"
+        radius="none"
+        onClick={() => alert('clicked')}
+      />
+      <Input
+        type="text"
+        placeholder="이메일을 입력해주세요"
+        inputType="signup"
+        width={10}
+        height={3}
+        fontSize="xs"
+        radius="none"
+        onKeydown={(e: React.KeyboardEvent<HTMLInputElement>) => {
+          if (e.key === 'Enter') alert('keydown');
+        }}
+      />
+    </div>
+  );
 }
