dcc9dd8d5383fa201e936e0bbab25051008b5e58
KIMHT98 <kht19684@gmail.com>
2024-07-24 06:27:56 +0900

    ADD: 달력 중간 커밋

--- ZIPPLZ_FE/src/App.tsx
+++ ZIPPLZ_FE/src/App.tsx
@@ -1,6 +1,7 @@
 import { Link, Outlet, Route, Routes } from 'react-router-dom';
 
 import Layout from './components/layout/Layout';
+import CalendarTest from './components/schedule/CalendarTest';
 import NotFound from './pages/common/NotFound';
 import Home from './pages/common/home/Home';
 import MyPage from './pages/common/mypage/MyPage';
@@ -12,7 +13,7 @@ export default function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="mypage" element={<MyPage />} />
-
+          <Route path="calendar" element={<CalendarTest />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
