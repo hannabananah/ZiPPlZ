d7293d223c3faa09ea35679a24ceced5447159c4
KIMHT98 <kht19684@gmail.com>
2024-07-24 03:19:20 +0900

    FEAT:스케줄러 공유문서함 디자인 반응형 구현

--- ZIPPLZ_FE/src/components/scheduler/sharedFile/SharedImg.tsx
+++ ZIPPLZ_FE/src/components/scheduler/sharedFile/SharedImg.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+import { useState } from 'react';
 import { FiPlusCircle } from 'react-icons/fi';
 import { LuMaximize2, LuMinimize2 } from 'react-icons/lu';
 import Modal from 'react-modal';
