515847098ed85403cee5fd3b062dfedfb8fd83bd
HannaKang <hanna0175@gmail.com>
2024-07-23 10:48:15 +0900

    Feat: 채팅 floating 버튼 UI

--- ZIPPLZ_FE/src/components/layout/FloatingChatButton.tsx
+++ ZIPPLZ_FE/src/components/layout/FloatingChatButton.tsx
@@ -1,27 +1,16 @@
 import { PiChatsCircleFill } from 'react-icons/pi';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import Circle from '@assets/gradient-circle.svg?react';
 
 export default function FloatingChatButton() {
   const navigate = useNavigate();
-  const location = useLocation();
-  const path = location.pathname;
-
-  // 로그인, 회원가입, 채팅룸에서는 안보이도록
-  if (
-    path === '/login' ||
-    path === '/signup' ||
-    path === '/chatroom' ||
-    path === '/chatlist'
-  )
-    return null;
 
   return (
     <>
       <button
         onClick={() => {
-          navigate('/chatlist');
+          navigate('/chatrooms');
         }}
         className="absolute right-4 drop-shadow-zp-deep bottom-24"
       >
