4c795916b036a425241993ac798fc2e2274790bc
HannaKang <hanna0175@gmail.com>
2024-07-25 08:06:55 +0900

    ADD: 화상채팅 UI 구현중

--- ZIPPLZ_FE/src/pages/common/chat/VideoRoom.tsx
+++ ZIPPLZ_FE/src/pages/common/chat/VideoRoom.tsx
@@ -6,19 +6,18 @@ export default function VideoRoom() {
   const { publisher, subscriber, leaveSession } = useOpenVidu();
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen text-white bg-gray-900">
-      <h1 className="mb-4 text-2xl font-bold">Video Chat</h1>
-      <div className="w-full max-w-4xl p-6 bg-gray-800 rounded-lg shadow-lg">
+    <div className="flex flex-col items-center justify-between min-h-screen p-4 text-white bg-gray-900">
+      <button
+        onClick={leaveSession}
+        className="px-4 py-2 mb-4 transition bg-red-600 rounded-md hover:bg-red-700"
+      >
+        화상채팅 나가기
+      </button>
+      <div className="flex flex-col w-full max-w-[600px] bg-gray-800 rounded-lg shadow-lg">
         <Session
           publisher={publisher as Publisher}
           subscriber={subscriber as Subscriber}
         />
-        <button
-          onClick={leaveSession}
-          className="px-4 py-2 mt-4 transition bg-red-600 rounded hover:bg-red-700"
-        >
-          화상채팅 나가기
-        </button>
       </div>
     </div>
   );
