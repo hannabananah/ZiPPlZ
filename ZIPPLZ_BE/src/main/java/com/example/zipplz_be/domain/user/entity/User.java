6e1a38a426d49f0efbcefb6e2346b8cfb6b34c74
Gaeun Lee <rk243812@gmail.com>
2024-07-24 18:03:37 +0900

    ì0724 임업로드

--- ZIPPLZ_BE/src/main/java/com/example/zipplz_be/domain/user/entity/User.java
+++ ZIPPLZ_BE/src/main/java/com/example/zipplz_be/domain/user/entity/User.java
@@ -11,7 +11,7 @@ import java.sql.Date;
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
