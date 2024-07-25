c3438348fd0f70c32b32f226a85795e8d36313ff
kht19684 <kht19684@gmail.com>
2024-07-26 03:18:48 +0900

    FEAT: 회원가입페이지 라우터 추가

--- ZIPPLZ_FE/src/App.tsx
+++ ZIPPLZ_FE/src/App.tsx
@@ -6,7 +6,9 @@ import Layout from './components/layout/Layout';
 import NotFound from './pages/common/NotFound';
 import ChatRoom from './pages/common/chat/ChatRoom';
 import Home from './pages/common/home/Home';
+import Login from './pages/common/login/LogIn';
 import MyPage from './pages/common/mypage/MyPage';
+import SignUp from './pages/common/signup/SignUp';
 import Schedule from './pages/user/Schedule';
 
 export default function App() {
@@ -19,6 +21,8 @@ export default function App() {
         <Route path="videoroom" element={<VideoRoom />} />
         <Route path="schedule" element={<Schedule />} />
         <Route path="*" element={<NotFound />} />
+        <Route path="member/join/:type/:order/:phrase" element={<SignUp />} />
+        <Route path="member/login" element={<Login />} />
       </Route>
     </Routes>
   );
