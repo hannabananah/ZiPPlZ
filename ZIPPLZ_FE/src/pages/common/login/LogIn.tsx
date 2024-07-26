1ca6d98231ea1cfac48d8d1357cd3c657a8950b3
kht19684 <kht19684@gmail.com>
2024-07-26 15:43:36 +0900

    FEAT: 시공자 회원가입시 작업 가능지역 선택 구현

--- ZIPPLZ_FE/src/pages/common/login/LogIn.tsx
+++ ZIPPLZ_FE/src/pages/common/login/LogIn.tsx
@@ -1,3 +1,111 @@
+import { useState } from 'react';
+import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa6';
+
+import Button from '@/components/common/Button';
+import Input from '@/components/common/Input';
+
 export default function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState<string>('');
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [showCheckPassword, setShowCheckPassword] = useState<boolean>(false);
+  const [password, setPassword] = useState<string>('');
+  function validatePassword(password: string): boolean {
+    if (!password) return true;
+    var passwordRegex = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*?_]).{8,16}$/;
+    return passwordRegex.test(password);
+  }
+  const handleCickEye = function () {
+    setShowPassword(!showPassword);
+  };
+  const handleCickCheckEye = function () {
+    setShowCheckPassword(!showCheckPassword);
+  };
+  function validateEmail(email: string): boolean {
+    let regex = new RegExp('[a-z0-9]+@[a-z]+.[a-z]{2,3}');
+    if (!email) return true;
+    return regex.test(email);
+  }
+  return (
+    <>
+      <div className="flex flex-col items-center gap-6">
+        <p className="font-bodoni text-zp-3xl">Zip plz</p>
+        <div className="text-center">
+          <p className="text-zp-xl font-bold">
+            완벽한 인테리어, 편리한 시공 스케줄링,
+          </p>
+          <p className="text-zp-xl font-bold">
+            나만의 공간을 더욱 특별하게 만드는 단 하나의 플랫폼
+          </p>
+        </div>
+      </div>
+      <div>
+        <p className="text-xl font-bold"></p>
+        <Input
+          type="email"
+          inputType="login"
+          placeholder="이메일을 입력하세요"
+          width="full"
+          height={2}
+          onChange={(e: React.ChangeEvent) => {
+            setEmail((e.target as HTMLInputElement).value);
+          }}
+          fontSize="xl"
+          radius="none"
+        />
+        {!validateEmail(email) && (
+          <p className="text-zp-2xs text-zp-red">
+            이메일 형식이 올바르지 않습니다.
+          </p>
+        )}
+      </div>
+      <div className="relative">
+        <Input
+          type={showPassword ? 'text' : 'password'}
+          inputType={
+            !password || validatePassword(password) ? 'signup' : 'error'
+          }
+          placeholder="비밀번호를 입력하세요"
+          width="full"
+          height={2}
+          onChange={(e: React.ChangeEvent) => {
+            setPassword((e.target as HTMLInputElement).value);
+          }}
+          fontSize="xl"
+          radius="none"
+        />
+        {showPassword ? (
+          <FaRegEyeSlash
+            className="absolute top-[0.5rem] right-[1rem]"
+            size={16}
+            onClick={handleCickEye}
+          />
+        ) : (
+          <FaRegEye
+            className="absolute top-[0.5rem] right-[1rem]"
+            size={16}
+            onClick={handleCickEye}
+          />
+        )}
+        {!validatePassword(password) && (
+          <p className="text-zp-2xs text-zp-red">
+            8~16자 영문 대소문자, 숫자, 특수문자를 사용하세요.
+          </p>
+        )}
+      </div>
+      <Button
+        buttonType="primary"
+        width="full"
+        height={4}
+        children="로그인하기"
+        fontSize="xl"
+        radius="btn"
+      />
+      <div className="flex w-full justify-between px-[15%]">
+        <p className="text-zp-md text-zp-gray  ml-[5%]">회원가입</p>
+        <p className="text-zp-md text-zp-gray  ">아이디/비밀번호 찾기</p>
+      </div>
+      <div className="w-full h-[4rem] bg-zp-main-color">소셜로그인</div>
+      <div className="w-full h-[4rem] bg-zp-main-color">소셜로그인</div>
+    </>
+  );
 }
--- ZIPPLZ_FE/src/pages/common/signup/worker/SignUpWorkerRegion.tsx
+++ ZIPPLZ_FE/src/pages/common/signup/worker/SignUpWorkerRegion.tsx
@@ -1,5 +1,109 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export default function SignUpWorkerRegion() {
-  return <div>SignUpWorkerRegion</div>;
+import Button from '@/components/common/Button';
+import { regionList } from '@/components/signup/regionData/region.js';
+
+interface Props {
+  setNext: React.Dispatch<React.SetStateAction<boolean>>;
+  setLink: React.Dispatch<React.SetStateAction<string>>;
+}
+export default function SignUpWorkerRegion({ setNext, setLink }: Props) {
+  const [selectedSido, setSelectedSido] = useState<number>(-1);
+  const [selectedGugun, setSelectedGugun] = useState<string[]>([]);
+
+  const handleButtonClick = (idx: number) => {
+    setSelectedSido(idx);
+  };
+  const [isFull, setIsFull] = useState<boolean>(false);
+
+  const handleGugunClick = (gugun: string) => {
+    setSelectedGugun((prev: string[]) => {
+      if (prev.includes(gugun)) {
+        return prev.filter((g: string) => g !== gugun);
+      } else {
+        return [...prev, gugun];
+      }
+    });
+  };
+  const handleDeleteClick = (region: string) => {
+    setSelectedGugun((prev: string[]) => {
+      return prev.filter((g) => g !== region);
+    });
+  };
+  useEffect(() => {
+    if (selectedGugun.length > 0) setNext(true);
+    else setNext(false);
+    if (selectedGugun.length === 8) {
+      setIsFull(true);
+    } else {
+      setIsFull(false);
+    }
+    setLink('/member/join/common/3/agree');
+  }, [selectedGugun]);
+  return (
+    <>
+      <p className="text-zp-xl font-bold">
+        작업 가능한 지역을 모두 선택해주세요.
+      </p>
+      <div className="w-full grid grid-cols-4 gap-4">
+        {regionList.map((item, idx) => (
+          <Button
+            buttonType={
+              selectedSido >= 0 && regionList[selectedSido].sido === item.sido
+                ? 'second'
+                : 'normal'
+            }
+            height={3.075}
+            radius="big"
+            fontSize="xl"
+            key={item.sido}
+            onClick={() => handleButtonClick(idx)}
+            children={item.sido}
+          />
+        ))}
+      </div>
+      {selectedSido >= 0 && (
+        <>
+          <hr className="text-zp-light-gray" />
+          <div className="w-full grid grid-cols-4 gap-4">
+            {regionList[selectedSido].guguns.map((gugun) => (
+              <Button
+                buttonType={selectedGugun.includes(gugun) ? 'second' : 'normal'}
+                height={3.075}
+                radius="big"
+                fontSize="xl"
+                key={gugun}
+                onClick={() => handleGugunClick(gugun)}
+                children={gugun}
+                disabled={isFull && !selectedGugun.includes(gugun)}
+              />
+            ))}
+          </div>
+        </>
+      )}
+      {selectedGugun.length > 0 && (
+        <>
+          <div className="relative w-full grid grid-cols-4 gap-4 p-4">
+            <div className="absolute top-0 left-0 w-full h-[18px] backdrop-blur-lg bg-gradient-to-b from-white to-transparent z-0"></div>
+            {selectedGugun.map((gugun) => (
+              <Button
+                buttonType="primary" // 또는 원하는 버튼 타입
+                height={3}
+                radius="big"
+                fontSize="lg"
+                key={`${regionList[selectedSido].sido} ${gugun}`}
+                children={`${regionList[selectedSido].sido} ${gugun}`}
+                onClick={() => {
+                  handleDeleteClick(`${gugun}`);
+                }}
+              />
+            ))}
+            <p className="text-zp-light-gray text-zp-lg absolute right-0 bottom-0">
+              {selectedGugun.length}/8
+            </p>
+          </div>
+        </>
+      )}
+    </>
+  );
 }
