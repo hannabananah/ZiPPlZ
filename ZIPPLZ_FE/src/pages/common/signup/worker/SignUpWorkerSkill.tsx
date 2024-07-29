97c2ffccac0492f510c73670b026ffeb892d872f
KIMHT98 <kht19684@gmail.com>
2024-07-30 01:42:28 +0900

    FEAT: 분야리스트 api연결

--- ZIPPLZ_FE/src/pages/common/signup/worker/SignUpWorkerSkill.tsx
+++ ZIPPLZ_FE/src/pages/common/signup/worker/SignUpWorkerSkill.tsx
@@ -1,77 +1,58 @@
 import React, { useEffect, useState } from 'react';
-import { HiChevronRight } from 'react-icons/hi2';
-import { setAppElement } from 'react-modal';
 
+import { getFields } from '@/apis/member/MemberApi';
 import Button from '@/components/common/Button';
-import Input from '@/components/common/Input';
 
-import value from './../../../../../svg.d';
-
-const skills: string[] = [
-  '철거',
-  '설비',
-  '샷시',
-  '목공',
-  '전기',
-  '욕실',
-  '타일',
-  '마루',
-  '도배',
-  '가구',
-];
+interface Field {
+  fieldName: string;
+  fieldCode: number;
+}
 interface Props {
   setNext: React.Dispatch<React.SetStateAction<boolean>>;
   setLink: React.Dispatch<React.SetStateAction<string>>;
 }
-interface CareerMap {
-  [key: string]: number;
-}
 
 export default function SignUpWorkerSkill({ setNext, setLink }: Props) {
-  const [selectedSkills, setSelectedSkills] = useState<string[]>([]);
-  const [career, setCareer] = useState<CareerMap>({});
-  const handleButtonClick = (skill: string) => {
-    if (selectedSkills.includes(skill)) {
-      setSelectedSkills((prev: string[]) =>
-        prev.filter((item: string) => item !== skill)
-      );
-      setCareer((prev) => {
-        const newCareer = { ...prev };
-        delete newCareer[skill];
-        return newCareer;
-      });
-    } else {
-      setSelectedSkills((prev) => [...prev, skill]);
-    }
+  const [fields, setFields] = useState<Field[]>([]);
+  const [selectedFields, setSelectedFields] = useState<Field[]>([]);
+  const fetchFields = async () => {
+    const response = await getFields();
+    setFields(response.data);
   };
-  const handleCareerChange = (skill: string, value: string) => {
-    setCareer((prev) => ({
-      ...prev,
-      [skill]: parseInt(value) || 0,
-    }));
+  useEffect(() => {
+    fetchFields();
+  }, []);
+  const handleButtonClick = (field: Field) => {
+    if (selectedFields.includes(field)) {
+      setSelectedFields((prev: Field[]) =>
+        prev.filter((item: Field) => item.fieldCode !== field.fieldCode)
+      );
+    } else {
+      setSelectedFields((prev: Field[]) => [...prev, field]);
+    }
   };
   useEffect(() => {
     setLink('/member/login');
-    if (selectedSkills.length == 0) {
+    if (selectedFields.length == 0) {
       setNext(false);
     } else {
       setNext(true);
     }
-  }, [selectedSkills]);
+  }, [selectedFields]);
   return (
     <>
       <div className="flex flex-col gap-6">
         <p className="text-zp-2xl font-bold">작업 분야 선택</p>
         <div className="w-full grid grid-cols-4 gap-4">
-          {skills.map((skill) => (
+          {fields.map((field) => (
             <Button
-              buttonType={selectedSkills.includes(skill) ? 'primary' : 'normal'}
+              buttonType={selectedFields.includes(field) ? 'primary' : 'normal'}
               height={3.075}
               radius="big"
               fontSize="xl"
-              key={skill}
-              onClick={() => handleButtonClick(skill)}
-              children={skill}
+              key={field.fieldCode}
+              onClick={() => handleButtonClick(field)}
+              children={field.fieldName}
             />
           ))}
         </div>
