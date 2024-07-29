9f327e066562c202f912080acf35fce1e1b45fd6
KIMHT98 <kht19684@gmail.com>
2024-07-30 00:57:50 +0900

    REFACTOR: env.local에서 BASE_URL 변수명 변경

--- ZIPPLZ_FE/src/apis/apiConstants.ts
+++ ZIPPLZ_FE/src/apis/apiConstants.ts
@@ -1,8 +1,13 @@
-export const BASE_URL: string = import.meta.env.BASE_URL;
+export const BASE_URL: string = import.meta.env.VITE_BASE_URL;
 
 export const END_POINT = {
+  //로그인 회원가입
   LOGIN: 'login',
   SIGNUP: 'users/join',
+  //기타
+  DEFAULT: 'default',
+  SIDO: 'default/sido',
+  GUGUN: (sidocode: number) => `default/sido/${sidocode}`,
 } as const;
 
 export const HTTP_STATUS_CODE = {
