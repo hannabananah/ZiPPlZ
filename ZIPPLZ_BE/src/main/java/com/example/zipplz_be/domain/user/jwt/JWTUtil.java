ad6e15b9ba104ec5c24af89a2a5f7b00671a7000
Gaeun Lee <rk243812@gmail.com>
2024-07-29 13:40:41 +0900

    FEAT: 도메인 수정

--- ZIPPLZ_BE/src/main/java/com/example/zipplz_be/domain/user/jwt/JWTUtil.java
+++ ZIPPLZ_BE/src/main/java/com/example/zipplz_be/domain/user/jwt/JWTUtil.java
@@ -26,7 +26,7 @@ public class JWTUtil {
     private final UserRepository userRepository;
     private SecretKey secretKey;
 
-    public JWTUtil(@Value("${spring.jwt.secret}") String secret, CustomUserDetailsService customUserDetailsService, UserRepository userRepository) {
+    public JWTUtil(@Value("{spring.jwt.secret}") String secret, CustomUserDetailsService customUserDetailsService, UserRepository userRepository) {
         secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
         this.customUserDetailsService = customUserDetailsService;
         this.userRepository = userRepository;
