8d8d233060d6d350c8b1366adeeae9697aa9f5ff
HannaKang <hanna0175@gmail.com>
2024-07-30 14:02:25 +0900

    REMOVE: 불필요한 파일 삭제

--- ZIPPLZ_BE/src/main/java/com/example/zipplz_be/domain/chatting/exception/ChatRoomNotFoundException.java
+++
@@ -1,5 +0,0 @@
-package com.example.zipplz_be.domain.chatting.exception;
-
-public class ChatroomNotFoundException extends RuntimeException {
-    public ChatroomNotFoundException(String message) { super(message); }
-}
