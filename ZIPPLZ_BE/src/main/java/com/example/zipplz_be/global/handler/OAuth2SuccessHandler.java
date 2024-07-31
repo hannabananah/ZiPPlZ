b781cfcda2f9da4212bbcf955d0492efc2a7b5a6
Gaeun Lee <rk243812@gmail.com>
2024-07-31 13:52:39 +0900

    FEAT: joinAfterSocialProcess 수정

--- ZIPPLZ_BE/src/main/java/com/example/zipplz_be/global/handler/OAuth2SuccessHandler.java
+++ ZIPPLZ_BE/src/main/java/com/example/zipplz_be/global/handler/OAuth2SuccessHandler.java
@@ -1,14 +1,15 @@
 package com.example.zipplz_be.global.handler;
 
+import com.example.zipplz_be.domain.user.dto.CustomOAuth2User;
 import com.example.zipplz_be.domain.user.entity.User;
 import com.example.zipplz_be.domain.user.jwt.JWTUtil;
 import com.example.zipplz_be.domain.user.repository.UserRepository;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -24,9 +25,20 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 
-        System.out.println("OAuth2 google login success!!!!!!");
-        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
-        String email = oAuth2User.getAttribute("email");
+        // 1. OAuth2User 가져오기
+        CustomOAuth2User customOAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+
+        String email = (String) customOAuth2User.getAttribute("email");
+
+        // 2-1. 만약 아직 회원가입을 하지 않은 유저라면
+        if (customOAuth2User.getIsNewUser()) {
+            HttpSession session = request.getSession();
+            session.setAttribute("email", email);
+            response.sendRedirect("http://localhost:5173/member/join/common/1/extra-agree");
+            return;
+        }
+
+        // 2-2. 이미 회원가입 한 유저라면
         User user = userRepository.findByEmail(email);
         int userSerial = user.getUserSerial();
         String role = user.getRole();
@@ -34,7 +46,6 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
         String token = jwtUtil.createJwt(email, userSerial, role);
 
         response.addHeader("Authorization", "Bearer " + token);
-        response.sendRedirect("http://localhost:5173/member/join/common/1/extrainfo");
-        System.out.println(token);
+        response.sendRedirect("http://localhost:5173/");
     }
 }
