9270e94ca2669bc1a0ebfd517f006e51d1475c58
Gaeun Lee <rk243812@gmail.com>
2024-08-02 09:43:51 +0900

    FEAT: mongoDB 연동

--- ZIPPLZ_BE/src/main/java/com/example/zipplz_be/ZipplzBeApplication.java
+++ ZIPPLZ_BE/src/main/java/com/example/zipplz_be/ZipplzBeApplication.java
@@ -3,7 +3,9 @@ package com.example.zipplz_be;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableMongoRepositories
 @EnableJpaAuditing
 @SpringBootApplication
 public class ZipplzBeApplication {
--- ZIPPLZ_BE/src/main/java/com/example/zipplz_be/domain/chatting/repository/ChatMessageRepository.java
+++ ZIPPLZ_BE/src/main/java/com/example/zipplz_be/domain/chatting/repository/ChatMessageRepository.java
@@ -1,7 +1,6 @@
 package com.example.zipplz_be.domain.chatting.repository;
 
 import com.example.zipplz_be.domain.chatting.entity.ChatMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
