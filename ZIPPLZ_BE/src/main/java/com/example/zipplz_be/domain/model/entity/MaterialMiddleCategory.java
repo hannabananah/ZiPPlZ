8822bd1e7e15b56347fc0a29b1608aa90fc6c295
Gaeun Lee <rk243812@gmail.com>
2024-08-06 15:13:32 +0900

    FEAT: 자재 대분류, 중분류 추가

---
+++ ZIPPLZ_BE/src/main/java/com/example/zipplz_be/domain/model/entity/MaterialMiddleCategory.java
@@ -0,0 +1,23 @@
+package com.example.zipplz_be.domain.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class MaterialMiddleCategory {
+    @Id
+    @Column(name = "major_code")
+    private int majorCode;
+
+    @Id
+    @Column(name = "middle_code")
+    private int middleCode;
+
+    @Column(name = "middle_name")
+    private String middleName;
+
+    @ManyToOne
+    @JoinColumn(name="major_code")
+    private MaterialMajorCategory major;
+}
