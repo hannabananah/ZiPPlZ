c2fbed7ec48adaadf79e4f96a53746bd40b72169
Gaeun Lee <rk243812@gmail.com>
2024-08-07 17:38:40 +0900

    FEAT: chatroomDetail 수정 2

--- ZIPPLZ_BE/src/main/java/com/example/zipplz_be/domain/chatting/dto/OtherUserInfoDTO.java
+++ ZIPPLZ_BE/src/main/java/com/example/zipplz_be/domain/chatting/dto/OtherUserInfoDTO.java
@@ -13,5 +13,7 @@ import lombok.NoArgsConstructor;
 public class OtherUserInfoDTO {
     private String name;
     private String location;
+    private String fieldName;
+    private boolean isCertificated;
     private File image;
 }
