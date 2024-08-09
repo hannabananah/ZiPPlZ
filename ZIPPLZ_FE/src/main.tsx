83b2585ea717b39e75edded3617563d05a75c7ef
kht19684 <kht19684@gmail.com>
2024-08-09 10:06:21 +0900

    ADD: 이미지 변환 인공지능 관련 파일 추가

--- ZIPPLZ_FE/src/main.tsx
+++ ZIPPLZ_FE/src/main.tsx
@@ -1,6 +1,7 @@
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
+import AppContextProvider from '@components/hooks/context';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import App from './App';
@@ -11,7 +12,9 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
-      <App />
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
     </BrowserRouter>
   </QueryClientProvider>
 );
