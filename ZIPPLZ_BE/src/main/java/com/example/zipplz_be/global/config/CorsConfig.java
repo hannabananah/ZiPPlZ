baeb21b2becbfaa70f7c42719d9db5032f596572
이주호 <jeuslee99@naver.com>
2024-08-14 10:01:18 +0900

    Update ZIPPLZ_BE/src/main/java/com/example/zipplz_be/global/config/CorsConfig.java

--- ZIPPLZ_BE/src/main/java/com/example/zipplz_be/global/config/CorsConfig.java
+++ ZIPPLZ_BE/src/main/java/com/example/zipplz_be/global/config/CorsConfig.java
@@ -29,6 +29,6 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
 
-        return new source;
+        return source;
     }
 }
