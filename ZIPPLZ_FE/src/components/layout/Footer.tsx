4ccf046ba1c2ff17967f22eee0fdd690a2bcf3e4
HannaKang <hanna0175@gmail.com>
2024-08-16 03:58:17 +0900

    FEAT: 푸터, 파비콘, 로고 추가

--- ZIPPLZ_FE/src/components/layout/Footer.tsx
+++ ZIPPLZ_FE/src/components/layout/Footer.tsx
@@ -1,94 +1,72 @@
+import Logo from '@assets/logo.svg?react';
+
 export default function Footer() {
   return (
-    <footer className="p-4 bg-white sm:p-6">
-      <div className="max-w-screen-xl mx-auto">
-        <div className="md:flex md:justify-between">
-          <div className="mb-6 md:mb-0">
-            <a href="#" className="flex items-center">
-              <img src="#" className="h-8 mr-3" alt="로고 이미지" />
-              <span className="self-center text-zp-lg whitespace-nowrap">
-                ZiPPlZ
-              </span>
+    <footer className="bottom-0 flex justify-center w-full pt-2 pb-4 border-t border-zp-light-gray bg-zp-sub-color">
+      <div className="w-full px-16">
+        <ul className="flex flex-col pb-3 text-left border-b text-zp-sm gap-y-1 border-zp-light-gray">
+          <li className="flex items-center -ml-2 text-black-color">
+            <Logo width={40} height={40} />
+            <a target="_blank" href="#">
+              <strong className="text-zp-black text-zp-xs">
+                개인정보처리방침
+              </strong>
             </a>
-          </div>
-          <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-            <div>
-              <h2 className="mb-6 text-sm font-semibold uppercase text-zp-gray">
-                Resources
-              </h2>
-              <ul className="text-gray-600">
-                <li className="mb-4">
-                  <a href="#" className="hover:underline">
-                    ZiPPlZ
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="hover:underline">
-                    Tailwind CSS
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h2 className="mb-6 text-sm font-semibold uppercase text-zp-gray">
-                Follow us
-              </h2>
-              <ul className="text-zp-gray">
-                <li className="mb-4">
-                  <a href="#" className="hover:underline ">
-                    Github
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="hover:underline">
-                    Discord
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h2 className="mb-6 text-sm font-semibold uppercase text-zp-gray">
-                Legal
-              </h2>
-              <ul className="text-zp-gray">
-                <li className="mb-4">
-                  <a href="#" className="hover:underline">
-                    Privacy Policy
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="hover:underline">
-                    Terms &amp; Conditions
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto" />
-        <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-zp-light-gray sm:text-center">
-            © 2024
-            <a href="#" className="hover:underline">
-              ZiPPlZ
+            &nbsp; &#124; &nbsp;
+            <a
+              target="_blank"
+              href="#"
+              className="text-zp-black text-zp-xs hover:underline"
+            >
+              이용약관
             </a>
-            . All Rights Reserved.
-          </span>
-          <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-            <a href="#" className="text-zp-light-gray hover:text-zp-gray">
-              {/* 이미지 */}
+          </li>
+          <li className="mt-1 text-zp-xs text-zp-gray">
+            <small className="text-zp-black">(주) </small>
+            ZiPPlZ
+          </li>
+          <li className="font-bold text-zp-xs text-zp-black">
+            대표자: 이가은 &nbsp;|&nbsp; 대표전화:
+            <a href="tel:0902-0817" className="hover:underline">
+              0902-0817
             </a>
-            <a href="#" className="text-zp-light-gray hover:text-zp-gray">
-              {/* 이미지 */}
+          </li>
+          <li className="text-zp-2xs text-zp-gray">
+            사업자등록번호:
+            <span className="text-zp-2xs text-zp-black">317-25-89091</span>
+          </li>
+          <li className="text-zp-2xs text-zp-gray">
+            이메일:
+            <a
+              href="mailto:zipplzService@gmail.com"
+              className="hover:underline"
+            >
+              zipplzService@gmail.com
             </a>
-            <a href="#" className="text-zp-light-gray hover:text-zp-gray">
-              {/* 이미지 */}
+          </li>
+          <li className="text-zp-gray text-zp-2xs">
+            찾아오시는 길: (06220)
+            <a
+              target="_blank"
+              href="https://map.naver.com/p/search/%EC%84%9C%EC%9A%B8%20%EA%B0%95%EB%82%A8%EA%B5%AC%20%ED%85%8C%ED%97%A4%EB%9E%80%EB%A1%9C%20212/address/14141986.3655684,4509210.2683534,%EC%84%9C%EC%9A%B8%ED%8A%B9%EB%B3%84%EC%8B%9C%20%EA%B0%95%EB%82%A8%EA%B5%AC%20%ED%85%8C%ED%97%A4%EB%9E%80%EB%A1%9C%20212,new?c=18.00,0,0,0,dh&isCorrectAnswer=true"
+              className="hover:underline"
+            >
+              서울특별시 강남구 언주로 508(역삼동 701번지)
             </a>
-            <a href="#" className="text-zp-light-gray hover:text-zp-gray">
-              {/* 이미지 */}
-            </a>
-          </div>
-        </div>
+          </li>
+          <li>
+            <p className="text-zp-2xs text-zp-light-gray">
+              copyright &copy; 2024 All rights reserved by ZiPPlZ.
+            </p>
+          </li>
+        </ul>
+        <p className="w-full pt-3 pb-16 text-left text-pretty text-zp-gray text-zp-3xs break-keep">
+          ZiPPlZ는 인테리어 시공업자와 고객을 잇는 중개 플랫폼입니다. ZiPPlZ는
+          시공업자와 고객 사이에 이루어지는 거래 내지 계약의 당사자가 아닙니다.
+          ZiPPlZ 플랫폼을 통하여 계약한 일정, 이에 대한 A/S 등 시공업자의
+          서비스에 관한 의무와 책임은 해당 서비스를 제공하는 시공업자에게
+          있습니다.
+        </p>
       </div>
     </footer>
   );
