f1eae15af9300f66900d3e48da8ad4a11c1a9ec9
kangsingu <qwerasd0411@naver.com>
2024-08-15 16:15:03 +0900

    REFACTOR: 구인구직 게시글 유저 주소 추가

---
+++ ZIPPLZ_BE/src/main/java/com/example/zipplz_be/domain/board/entity/BoardUserAddress.java
@@ -0,0 +1,22 @@
+package com.example.zipplz_be.domain.board.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BoardUserAddress {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="board_user_address_serial")
+    private int boardUserAddressSerial;
+    @ManyToOne
+    @JoinColumn(name="board_serial")
+    private Board boardSerial;
+    @Column(name="user_address")
+    private String userAddress;
+}
