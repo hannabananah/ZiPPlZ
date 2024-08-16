8d0bbe2d52bde444d84c8af0c842c283425b642e
이주호 <jeuslee99@naver.com>
2024-08-16 09:13:29 +0900

    Update ZIPPLZ_BE/src/main/java/com/example/zipplz_be/domain/material/repository/MaterialSearchRepository.java

--- ZIPPLZ_BE/src/main/java/com/example/zipplz_be/domain/material/repository/MaterialSearchRepository.java
+++ ZIPPLZ_BE/src/main/java/com/example/zipplz_be/domain/material/repository/MaterialSearchRepository.java
@@ -1,13 +1,13 @@
-package com.example.zipplz_be.domain.material.repository;
+// package com.example.zipplz_be.domain.material.repository;
 
-import com.example.zipplz_be.domain.material.entity.ElasticMaterial;
-import org.springframework.data.elasticsearch.annotations.Query;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
-import java.util.List;
+// import com.example.zipplz_be.domain.material.entity.ElasticMaterial;
+// import org.springframework.data.elasticsearch.annotations.Query;
+// import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+// import org.springframework.stereotype.Repository;
+// import java.util.List;
 
-@Repository
-public interface MaterialSearchRepository extends ElasticsearchRepository<ElasticMaterial, Integer> {
-    @Query("{\"match\": {\"materialName\": \"?0\"}}")
-    List<ElasticMaterial> findByMaterialName(String materialName);
-}
+// @Repository
+// public interface MaterialSearchRepository extends ElasticsearchRepository<ElasticMaterial, Integer> {
+//     @Query("{\"match\": {\"materialName\": \"?0\"}}")
+//     List<ElasticMaterial> findByMaterialName(String materialName);
+// }
