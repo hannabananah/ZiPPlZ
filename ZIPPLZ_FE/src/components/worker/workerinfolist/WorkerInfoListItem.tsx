64fc23cfa14c28142b302260e18c54ecdfe84d00
kht19684 <kht19684@gmail.com>
2024-08-18 16:57:22 +0900

    FIX:시공자리스트&구인글관련 수정

--- ZIPPLZ_FE/src/components/worker/workerinfolist/WorkerInfoListItem.tsx
+++ ZIPPLZ_FE/src/components/worker/workerinfolist/WorkerInfoListItem.tsx
@@ -19,23 +19,26 @@ export default function WorkerInfoListItem({ worker }: Props) {
       }
     >
       {worker.img ? (
-        <img className="w-full h-[35%] rounded-t-zp-radius-big" src="" />
+        <img
+          className="w-full h-[40%] rounded-t-zp-radius-big"
+          src={worker.img}
+        />
       ) : (
-        <div className="flex items-center justify-center w-full h-full">
-          <NoImage width={180} />
+        <div className="flex items-center justify-center w-full h-[40%]">
+          <NoImage height="full" />
         </div>
       )}
       <div className="flex flex-col w-full p-2">
         <div className="flex items-center gap-2">
-          <p className="font-bold text-zp-xs">{worker.user_name}</p>
-          <p className="text-zp-2xs text-zp-light-gray">{worker.birth_date}</p>
+          <p className="font-bold text-zp-2xs">{worker.user_name}</p>
+          <p className="text-zp-3xs text-zp-light-gray">{worker.birth_date}</p>
           {worker.certificated_badge > 0 && (
             <CertificatedBadge width={16} height={16} />
           )}
         </div>
-        <p className="text-zp-2xs text-zp-light-gray">{worker.temperature}</p>
+        <p className="text-zp-3xs text-zp-light-gray">{worker.temperature}</p>
         {worker.locations.map((region: string) => (
-          <p className="text-zp-2xs text-zp-light-gray">{region}</p>
+          <p className="text-zp-3xs text-zp-light-gray">{region}</p>
         ))}
         <p className=" text-zp-3xs">
           {worker.field_name} | &nbsp;경력 {worker.career}년
