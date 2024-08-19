bf465bee92e90ded18f4fe0c85de4a51888a4f51
김현태 <kht19684@gmail.com>
2024-08-19 15:58:34 +0900

    FIX: 이미지 변환 페이지 이동 url변경

--- ZIPPLZ_FE/src/pages/common/home/Home.tsx
+++ ZIPPLZ_FE/src/pages/common/home/Home.tsx
@@ -55,7 +55,8 @@ export default function Home() {
   }, []);
 
   const handleClickImageChange = () =>
-    (window.location.href = `https://43.201.5.30:8080?user=${loginUser?.userSerial}`);
+    // (window.location.href = `https://43.201.5.30:8080?user=${loginUser?.userSerial}`);
+    navigate(`/image-change/{loginUser?.userSerial`)
     // (window.location.href = `https://zipplz.site/dev`);
   const fetchWorks = async () => {
     const response = await getWorksByUser();
